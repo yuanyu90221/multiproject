2bd7b8163586862eafeb95c6390df54b2e30bd37
yuanyu90221 <yuanyu90221@gmail.com>
2016-09-18 16:40:12 +0800

    使用hbs module帶入host name

---
+++ modules/socketCtrlFatory.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'socketOn': function(io, uuid, all_inning, db, all_game_guid, all_socket, player_g, player_i ,dbAccessModule){
+		
+	}	
+}
--- test.js
+++
@@ -1,5 +0,0 @@
-var test = {
-	name : 'json'
-};
-
-exports.test = test;
